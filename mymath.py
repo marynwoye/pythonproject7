beb087d99b394741dfb6df963b45260b312f00ac
nwoye <122365633@umail.ucc.ie>
2024-11-08 12:09:26 +0000

    Running Intial Tests

--- mymath.py
+++ mymath.py
@@ -1,17 +1,14 @@
 def add_three(a, b, c):
-    return a+b+c
+    return -1
 
 def subtract_three(a, b, c):
-    return a-b-c
+    return -1
 
 def multiply_three(a, b, c):
-    return a*b*c
+    return -1
 
 def divide_three(a, b, c):
-    return a/b/c
+    return -1
 
 def power(a, b):
-    return a**b
-# hi there
-# hey
-# extra comment
+    return -1
